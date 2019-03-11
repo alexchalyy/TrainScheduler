19a47d736d2fcd9e89cfc3dc49a662f8f8f9015c
alex chalyy <alexchalyy@gmail.com>
2019-03-10 21:46:48 -0400

    table rows are not dynamically adding:(

--- assets/javascript/Moment.js
+++ assets/javascript/Moment.js
@@ -22,40 +22,176 @@ var config = {
 
 firebase.initializeApp(config);
 
-//var v = document.getElementById('another_title');
-//var dbRef = firebase.database().ref().child('text');
+var database = firebase.database();
 
-//var dataRef = firebase.database();
+//----------------------------------------------------------------------------------------------------
 
-// On Click
-$("#clear").on("click", function () {
+function addRow() {
 
-  // Add 1 to clickCounter
-  frequency = "hello";
+  //  This function adds a row to html file dynamically every time new row is added.
 
-  // **** Store Click Data to Firebase in a JSON property called clickCount *****
-  // **** Note how we are using the Firebase .set() method ****
-  // **** .ref() refers to the path you want to save your data to
-  // **** Since we left .ref() blank, it will save to the root directory
-  database.ref().set({
-    frequency: frequency
-  });
+    // Create the new row
+    console.log("addRow!");
+    var newRow = $("<tr>").append(
+      $("<td>").text(train_name),
+      $("<td>").text(destination),
+      $("<td>").text(frequency),
+      $("<td>").text(next_arrival),
+      $("<td>").text(minutes_away),
+    );
+  
+    // Append the new row to the table
+    $("#t > tbody").append(newRow);
 
-  // Now! go to https://fir-click-counter-7cdb9.firebaseio.com/ to see the impact to the DB
+}
+
+//-----------------------------------------------------------------------------------------------------
+
+database.ref().on("child_added", function (document) {
+
+  //  This function loads all train infromation from firebase, logs it, and displays it on html page.
+
+  //console.log(document.val());
+
+  // Store everything from firebase into a variables.
+
+  train_name = document.val().TrainName;
+  destination = document.val().Destination;
+  frequency = document.val().Frequency;
+  next_arrival = document.val().NextArrival;
+  minutes_away = document.val().MinutesAway;
+
+  // Logs everything extracted from firebase;
+
+  console.log("Train name: +" + train_name);
+  console.log("Destination: +" + destination);
+  console.log("Frequency: " + frequency);
+  console.log("Next arrival: " + next_arrival);
+  console.log("Minutes away: " + minutes_away);
+
+  addRow();
 });
 
-/*
-    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-    dataRef.ref().on("child_added", function(childSnapshot) {
+//------------------------------------------------------------------------------------------------------
 
-        // Log everything that's coming out of snapshot
-        console.log(childSnapshot.val().TrainName);
-        console.log(childSnapshot.val().Destination);
-        console.log(childSnapshot.val().Frequency);
-        console.log(childSnapshot.val().NextArrival);
-        console.log(childSnapshot.val().MinutesAway);
+function isNormalInteger(str) {
 
-        // Handle the errors
-      }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      }); */
+  //  This function returns true if str is a positive integer.
+
+  var n = Math.floor(Number(str));
+
+  return n !== Infinity && String(n) === str && n >= 0;
+}
+
+//------------------------------------------------------------------------------------------------------
+
+function DisplayCurrentTime() {
+
+  //  This function returns current military time.
+
+  var date = new Date();
+  var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  time = hours + ":" + minutes;
+  return time;
+};
+
+//------------------------------------------------------------------------------------------------------
+
+function validTIme(time) {
+
+  //  This function returns true if the received string is valid military time.
+
+  return (parseInt(time[0]) != NaN && parseInt(time[1]) != NaN && parseInt(time[3]) != NaN && parseInt(time[4]) != NaN);
+
+}
+
+//------------------------------------------------------------------------------------------------------
+
+function CalculateArrival() {
+
+  //  This function caclulates next arrival time and minutes away from arrival. This function was created with Doug's help.
+
+  var trainTime = moment(first_train_time, "hh:mm").subtract(1, "years");
+  var minuteDifference = moment().diff(moment(trainTime), "minutes");
+  var remainder = minuteDifference % frequency;
+
+  minutes_away = frequency - remainder;
+  var nextTrain = moment().add(minutes_away, "minutes");
+  next_arrival = moment(nextTrain).format("hh:mm");
+
+  console.log("Arrival time: " + next_arrival);
+  console.log("Arrival minutes: " + minutes_away);
+
+}
+
+//------------------------------------------------------------------------------------------------------
+
+function StoreTrainSchedule() {
+
+  //  This function stores added train schedule into firebase.
+
+  var newTrain = {
+    TrainName: train_name,
+    Destination: destination,
+    Frequency: frequency,
+    NextArrival: next_arrival,
+    MinutesAway: minutes_away
+  }
+
+  database.ref().push(newTrain);
+
+  console.log("Next train schedule row is successfully added.");
+}
+
+//------------------------------------------------------------------------------------------------------
+
+$("#sub").on("click",
+
+  function (event) {
+
+    //  This function loads information from submit form, stores it firebase, and displays it on screen.
+
+    event.preventDefault();
+
+    //  This gets values from the text boxes.
+
+    train_name = $("#TrainName").val().trim();
+    destination = $("#Destination").val().trim();
+    frequency = $("#Frequency").val().trim();
+    first_train_time = $("#FirstTrainTime").val().trim();
+
+    //  This makes sure that the user enters all values into text boxes before processing the information.
+
+    if (train_name == "" || destination == "" || frequency == "" || first_train_time == "") {
+      alert("Please enter all values to submit train time!");
+    } else if (first_train_time.length < 5 || !validTIme(first_train_time) || first_train_time[2] != ":") {
+      alert("Please enter military time only (HH:MM)!");
+    } else if (!isNormalInteger(frequency)) {
+      alert("Please enter valid frequency.");
+    } else if (!isNaN(train_name) || !isNaN(destination)) {
+      alert("Please enter valid non-numerical names for train name and destination.");
+    }
+    else {
+      console.log("Current time: " + DisplayCurrentTime());
+      CalculateArrival();
+      StoreTrainSchedule();
+      addRow();
+    }
+
+    //  This logs the values from the text boxes.
+
+    console.log("Train Name: " + train_name);
+    console.log("Destination: " + destination);
+    console.log("Frequency: " + frequency);
+    console.log("First train time: " + first_train_time);
+
+    //  This clears the text boxes after the user clicks submit.
+
+    $("#TrainName").val("");
+    $("#Destination").val("");
+    $("#Frequency").val("");
+    $("#FirstTrainTime").val("");
+  }
+);
+
